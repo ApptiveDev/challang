1a37766ce6e4430851952d91a0994daf35d2bba8
Gahyeon <best11gh@naver.com>
2025-03-30 03:49:54 +0900

    Feat: project init

---
+++ src/main/java/com/challang/backend/ChallangBackendApplication.java
@@ -0,0 +1,13 @@
+package com.challang.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChallangBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChallangBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/challang/backend/ChallangBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.challang.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallangBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
