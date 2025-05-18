ca0fc1ab11b3daf2a5638e3ee91125d14ea4f7c6
Gahyeon <best11gh@naver.com>
2025-05-19 03:22:59 +0900

    feat: 인증 로직 구현 및 토큰 관련 클래스 추가

---
+++ src/main/java/com/challang/backend/auth/constant/AuthConstant.java
@@ -0,0 +1,12 @@
+package com.challang.backend.auth.constant;
+
+public class AuthConstant {
+    public static final String AUTHORIZATION = "Authorization";
+    public static final String BEARER = "Bearer ";
+    public static final String ACCESS_TOKEN = "access_token";
+    public static final String REFRESH_TOKEN = "refresh_token";
+    public static final String CATEGORY = "category";
+    public static final String ROLE = "role";
+//    public static final String EMAIL = "email";
+    public static final String KAKAO = "kakao";
+}
