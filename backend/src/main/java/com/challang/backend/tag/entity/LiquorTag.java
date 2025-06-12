19f5daf9ea481ac99689b116308cfe8299cf0433
Gahyeon <best11gh@naver.com>
2025-06-12 16:03:27 +0900

    feat: 태크 및 주류-태그 연결 엔티티 추가

---
+++ backend/src/main/java/com/challang/backend/tag/entity/LiquorTag.java
@@ -0,0 +1,29 @@
+package com.challang.backend.tag.entity;
+
+import com.challang.backend.liquor.entity.Liquor;
+import com.challang.backend.util.entity.BaseEntity;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "liquor_tag")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class LiquorTag extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "liquor_id", nullable = false)
+    private Liquor liquor;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "tag_id", nullable = false)
+    private Tag tag;
+
+    @Column(name = "is_core", nullable = false)
+    private boolean isCore; // 핵심적 태그인지
+
+}
